e20277809aa8f28ba9478811947142c6012643c2
hlebos01 <hlebos847@gmail.com>
2025-12-14 11:50:46 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherApp from './components/WeatherApp';
+import City from './components/City';
+
+function App() {
+  return (
+    <>
+    <WeatherApp/>
+    </>
+  );
+}
+
+export default App;
